047e4ef9c6788317eeb0a029b553c821689a7ca7
AshaS591 <ashas17205@gmail.com>
2024-09-16 10:25:56 +0530

    added age program

--- firstprogram.py
+++ firstprogram.py
@@ -1,7 +1,7 @@
 print('Hello Python Lovers')
 #wap to check a person eligible to vote or not
 age=int(input('Enter age :'))
-if age>18:
+if age>=18:
     print('Eligible to vote')
 else:
     print('Not eligible to vote')
