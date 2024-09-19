a8874ef96aa23e8d3115b5985fa6d7f6bc6fd5ad
AshaS591 <ashas17205@gmail.com>
2024-09-19 19:49:17 +0530

    sep and end

---
+++ sep_and_end.py
@@ -0,0 +1,19 @@
+# sep is the separator which separates variables by space by default in the print function ,if we want separate variables or values specifically we can do that using the sep 
+print(10,20) #by default separated by space
+name='asha'
+print(name,'hello',sep='@')
+print(name,'means having desire',sep=' : ')
+print('asha','anu','haripriya','earth','sky','rain',sep=' hey ')
+print('🤨🙂😑🤔🤩☺️😚😥😮😶🙄😏',sep='%%')
+print("🤩",'😍','😁','😂','🤣','🥰','😅','🤗','🙂',sep='100%')
+print('python','sql','html','css','javascript',sep='\n') #\n means new line
+
+#end is used to add specific character or sentence or whatever that we want to add at the end after printing all the values in the print function 
+# by default end adds new line character
+print('hello!')
+print('❤️ India❤️')
+print('I LOVE ',end='')
+print('❤️ India❤️ ',end=' 👌\n')
+print('🤨🙂😑🤔🤩☺️😚😥😮😶🙄😏',end='💕\n')
+print("🤩",'😍','😁','😂','🤣','🥰','😅','🤗','🙂','these are types of smiles hidden behind every beautiful and kind hearted persons' ,sep=' smile ',end=' always keep smile whatever may be the situation face with smile\n')
+print('hey','hey','hey','hey','hey','hey',sep='****',end='-----🥰-----')
