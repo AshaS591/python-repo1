ac827c76c1e65bd9fb5f6776523bc1e73753a4a2
AshaS591 <ashas17205@gmail.com>
2024-09-19 19:54:56 +0530

    logical_operators

---
+++ logical_operators.py
@@ -0,0 +1,29 @@
+# and ,or ,not --->returns bool 
+
+# and 
+print(-10<-2 and 90>89.99)
+str1='asha'
+srt2='ASHA'
+print(str1<srt2 and str1<'zoo')
+
+#########################################################
+#########################################################
+
+# or
+print(-10<-2 or 90>89.99)
+str1='asha'
+srt2='ASHA'
+print(str1<srt2 or str1<'zoo')
+
+#########################################################
+#########################################################
+
+# not
+print(not(-10<-2 and 90>89.99))
+str1='asha'
+srt2='ASHA'
+print(not(str1<srt2 and str1<'zoo'))
+print(not(-10<-2 or 90>89.99))
+str1='asha'
+srt2='ASHA'
+print(not(str1<srt2 or str1<'zoo'))
