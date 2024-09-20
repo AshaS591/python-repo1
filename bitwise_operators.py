ddc817758ba231a984f26aac9e017119817264c6
AshaS591 <ashas17205@gmail.com>
2024-09-20 21:36:50 +0530

    Bitwise Operators

---
+++ bitwise_operators.py
@@ -0,0 +1,29 @@
+#right shift op ---left operand//(2**right oprand)
+print(24>>3)
+print(2400>>8) 
+
+#left shift op --left operand * (2**right oprand)
+print(21<<3)
+print(10<<6)
+print(11<<4)
+
+# & -bitwise and
+print(10 & 20)
+print(20&60)
+print(5 & 15)
+
+# | -bitwise or op
+print(20 | 90)
+print(15 |10)
+print(72 | 22)
+
+# ~ -bitwise not op
+print(~(20))
+print(~(-11))
+print(~(9))
+print(~(22))
+
+# ^ -bitwise xor op
+print(10^20)
+print(25^34)
+print(23^12)
