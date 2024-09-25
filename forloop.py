7719f4581322b6c58c8ab5b27601408d70f641c2
AshaS591 <ashas17205@gmail.com>
2024-09-25 21:13:31 +0530

    added some more programs

--- forloop.py
+++ forloop.py
@@ -30,3 +30,38 @@ for char in string:
     count+=1
 print(f"Length of string {string} is {count}")
 
+#5.wap to display a largest number in a given collection
+collection=eval(input('Enter a collection of numbers :'))
+max=0
+for num in collection:
+    if num>max:
+        max=num
+print(f"Largest number of a collection is {max}")
+
+#6.wap to print range of natural numbers entered by the user using for loop
+num=int(input('Enter the ending number :'))
+for nums in range(num):
+    print(nums)
+
+#7.wap to print even numbers by using range entered by the user
+last_num=int(input('Enter a number :'))
+for even in range(last_num):
+    if even%2==0:
+        print(even)
+
+#8.wap to print square of a numbers by using range entered by the user
+last_num=int(input('Enter a last number :'))
+for num in range(last_num):
+    print(f"Square of {num} is {num**2}")
+
+#9.wap to print multiples of 5 and 10 by using range entered by the user
+last_num=int(input('Enter a last number :'))
+for num in range(last_num):
+    if num%5==0 and num%10==0:
+        print(f"{num} is multiple of both 5 and 10")
+
+#10.wap to print countdown of a game using for loop
+start=int(input("Eneter a countdown starting number :"))
+for count in range(start,-1,-1):
+    print(count)
+
