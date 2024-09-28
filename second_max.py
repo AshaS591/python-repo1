7f7e3cfdd39e50f51a40e628c342c266a745096b
AshaS591 <ashas17205@gmail.com>
2024-09-28 20:09:58 +0530

    updated

--- second_max.py
+++ second_max.py
@@ -1,6 +1,7 @@
-num1=int(input('Enter num1 :'))
-num2=int(input('Enter num2 :'))
-num3=int(input('Enter num3 :'))
+#1.wap to print second maximum number among 3 numbers
+num1 = int(input('Enter num1 :'))
+num2 = int(input('Enter num2 :'))
+num3 = int(input('Enter num3 :'))
 if num1>num2:
     if num1>num3:
         if num2>num3:
@@ -17,3 +18,9 @@ else:
             print(f"{num3} is second greatest number")
     else:
         print(f"{num2} is second greatest number")
+
+#2.wap to print second maximum number among 4 numbers
+num1 = int(input('Enter num1 :'))
+num2 = int(input('Enter num2 :'))
+num3 = int(input('Enter num3 :'))
+num4 = int(input('Enter num3 :'))
