acf6ba02647abf94af001e4de9a4de4490bf7dcd
AshaS591 <ashas17205@gmail.com>
2024-10-24 21:00:49 +0530

    one more program using lambda function

--- lambda_func.py
+++ lambda_func.py
@@ -19,3 +19,6 @@ print(length([10,'word',10+0j,90.12,'100']))
 greatest_among_two=lambda num1,num2:num1 if num1>num2  else num2
 print(greatest_among_two(10,20))
 
+# wap to return one more even number if the user entered number is even number otherwise return one more odd number
+other_even_or_odd=lambda num:(num,num+2) if num%2==0 else (num,num+2)
+print(other_even_or_odd(11))
