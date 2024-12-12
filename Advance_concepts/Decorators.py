101b4e80a8bad424118becf25b06c0ba1b0fb55b
AshaS591 <ashas17205@gmail.com>
2024-12-12 21:21:20 +0530

    number palindrome

--- Advance_concepts/Decorators.py
+++ Advance_concepts/Decorators.py
@@ -33,21 +33,51 @@ def display(n):
 display()
 ##################################################################################################
 
+"""WAP to print factorial of a number using decorator"""
 
 def number(num):
     def whole_number(func_name):
         def calculate(*args,**kwargs):
-            print(f"Calculating factorial of number : {num}")
-            Factorial=func_name(num)
-            print(f"Factorial of number : {num} is {Factorial}")
+            try:
+                print(f"Calculating factorial of number : {num}")
+                Factorial=func_name(*args,**kwargs)
+            except Exception as msg:
+                print(msg)
+            else:
+                print(f"Factorial of number : {num} is {Factorial}")
         return calculate
     return whole_number
 
-@number(5)
+@number(6)
 def factorial(num:int):
     fact=1
     for num in range(1,num+1):
         fact*=num
     return fact
 
-factorial()
+factorial(6)
+
+##################################################################################################
+
+"""WAP to print reverse of a number using decorator"""
+
+def number(func_name):
+        def display_reverse(*args,**kwargs):
+            try:
+                rev=func_name(*args,**kwargs)
+            except Exception as msg:
+                print(msg)
+            else:
+                print(f"Reverse of number is {rev}")
+        return display_reverse
+
+@number
+def reverse(num:int):
+    rev_num=0
+    while num!=0:
+        last_digit=num%10
+        rev_num=rev_num*10+last_digit
+        num//=10
+    return rev_num
+
+reverse(456)
