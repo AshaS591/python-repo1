dc115dd0e00fab2fb20c63fd894ed8774390bf59
AshaS591 <ashas17205@gmail.com>
2024-12-14 08:31:16 +0530

    two more programs on exception handling

--- newpatterns.py
+++ newpatterns.py
@@ -1,12 +1,12 @@
-# #A
-# n=int(input('enter n value :'))
-# for row in range(n):
-#     for col in range(n):
-#         if (row==0 and (col>0 and col<n-1))  or (col==0 and (row>0 and row<=n-1)) or (col==n-1 and (row>0 and row<=n-1)) or row==n//2:
-#             print('*',end=" ")
-#         else:
-#             print(' ',end=" ")
-#     print()
+#A
+n=int(input('enter n value :'))
+for row in range(n):
+    for col in range(n):
+        if (row==0 and (col>0 and col<n-1))  or (col==0 and (row>0 and row<=n-1)) or (col==n-1 and (row>0 and row<=n-1)) or row==n//2:
+            print('*',end=" ")
+        else:
+            print(' ',end=" ")
+    print()
 
 # #B
 # n=int(input('enter n value :'))
