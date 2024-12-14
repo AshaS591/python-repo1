58562b8e784cf5118a5a548ef499166013ed175d
AshaS591 <ashas17205@gmail.com>
2024-12-14 22:05:22 +0530

    .....

--- Mini_Projects/calculator.py
+++ Mini_Projects/calculator.py
@@ -1,4 +1,4 @@
-
+import os
 def add(num1,num2):
     print(f'Sum of {num1} and {num2} is {num1+num2}')
     return num1+num2
@@ -20,29 +20,33 @@ def floor_division(num1,num2):
 
 operations={'+':add,'-':diff,'*':product,'/':true_division,'%':remainder,'//':floor_division}
 
-choice=input("...Here you can perform arithmetic operations ...\nDo you like to perform any operation,\nif yes type 'y' otherwise type 'n' to exit :\n")
-if choice=='y':
-        num1=int(input('Enter first number :\n'))  
+# choice=input("...Here you can perform arithmetic operations ...\nDo you like to perform any operation,\nif yes type 'y' otherwise type 'n' to exit :\n")
+# if choice=='y':
+def one_more_check():
+    num1=int(input('Enter first number :\n')) 
+    for symbol in operations:
+            print(symbol)
+    flag=True
+    while flag: 
+        operator=input('Pick an operation :\n')
+        num2=int(input('Enter second number :\n'))
+        calcy=operations[operator]
+        output=calcy(num1,num2)
+        print(f"{num1} {operator} {num2} = {output}")
+        one_more_time=input(f"Enter '1' for continue operation with {output} or '2' for start new operation or '3' for exit")
+        if one_more_time=='1':
+            output
+        elif one_more_time=='2':
+            flag=False
+            os.system('cls')
+            one_more_check()
+        else:
+            flag=False
+            print('-----end-----')
+one_more_check()
 
-        def one_more_check():
-            
-            global num1
-            print('+\n-\n*\n/\n%\n//')
-            operator=input('Pick an operation :\n')
-            num2=int(input('Enter second number :\n'))
-            for operator in operations:
-                result=operations[operator](num1,num2)
-                print(f'Do you want to perform operation on {result}\n')
-                while True:
-                    one_more_time=input("Type '1' for yes, '2' for exit :\n")
-                    if one_more_time=='1':
-                        num1=result
-                        one_more_check()
-                    elif one_more_time=='2':
-                        break
 
-        one_more_check()
-
+        
 
 
 
