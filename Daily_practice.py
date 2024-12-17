74bc5d6e2893d52522567aa46b8c2594ab94a42a
AshaS591 <ashas17205@gmail.com>
2024-12-17 23:00:53 +0530

    1 more

--- Daily_practice.py
+++ Daily_practice.py
@@ -281,3 +281,13 @@ for row in range(5):
         print(row+1,end=' ')
     print()
 
+'''
+4 4 4 4 4
+3 3 3 3 3 
+2 2 2 2 2
+1 1 1 1 1
+'''
+for row in range(6,0,-1):
+    for col in range(6):
+        print(row,end=' ')
+    print()
