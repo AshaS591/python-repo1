3efbf232b0203f3839b6f1450f258766ad19fd27
AshaS591 <ashas17205@gmail.com>
2024-12-17 22:57:10 +0530

    added one more

--- Daily_practice.py
+++ Daily_practice.py
@@ -269,3 +269,14 @@ for row in range(5):
         count+=1
         print(count,end=' ')
     print( )
+print()
+'''
+1 1 1 1 1
+2 2 2 2 2
+3 3 3 3 3
+4 4 4 4 4
+'''
+for row in range(5):
+    for col in range(5):
+        print(row+1,end=' ')
+    print()
