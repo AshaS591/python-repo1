620f6b6c55297da6b98b4762f0509ae97de3fffa
AshaS591 <ashas17205@gmail.com>
2024-12-18 15:50:42 +0530

    one more program

--- Advance_concepts/comprehension.py
+++ Advance_concepts/comprehension.py
@@ -13,12 +13,19 @@ print(even_list)
 # 3. wap to create list ,if item last second character is vowel by taking another collection from the user using comprehension
 try:
     items=eval(input('Enter a collection :'))
+    new_list=[item  for item in items if item[-2] in 'aeiouAEIOU']
 except Exception as err_msg:
     print(err_msg)
 else:
-    try:
-        new_list=[item  for item in items if item[-2] in 'aeiouAEIOU']
-    except Exception as err_msg:
-        print(err_msg)
-    else:
-        print(new_list)
+    print(new_list)
+
+# 4. wap to create a list by taking another number collection from user if num is even append its square ,if num is odd append cube of that number
+
+try:
+    numbers=eval(input('Enter a number collection :'))
+    new_numbers=[num**2 if num%2==0 else num**3 for num in numbers ]
+except Exception as err_msg:
+    print(err_msg)
+else:
+    print(new_numbers)
+
