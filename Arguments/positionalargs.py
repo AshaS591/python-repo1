b7a7e2d9fef23724283ff427bdccdf4a71090e7b
AshaS591 <ashas17205@gmail.com>
2024-12-19 22:50:26 +0530

    positional arguments

--- Arguments/positionalargs.py
+++ Arguments/positionalargs.py
@@ -1,4 +1,8 @@
-def add(num1,num2=2):
+def add(num1,num2):
     return num1+num2
-res=add(20,num1=30)
+res=add(20,30)
 print(res)
+
+def details(name,age):
+    print(f"Name :{name},Age :{age}")
+details(20,'asha')
