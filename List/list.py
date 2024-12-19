2e8a1100891ae3c0afc56f62800eb4bbc4e0a503
AshaS591 <ashas17205@gmail.com>
2024-12-19 23:16:01 +0530

    all builtin methods of list

--- List/list.py
+++ List/list.py
@@ -7,5 +7,30 @@ flowers=['jasmine','lily','sun flower','marrigold']
 new_flowers=flowers.copy() #copy method
 print(new_flowers)
 
-new_flowers.remove('marrigold')
+new_flowers.remove('marrigold') #remove method
 print(new_flowers)
+
+new_flowers.clear() #clear method
+print(new_flowers)
+
+flowers.extend(['rose','lotus','hibiscus']) #extend method
+print(flowers)
+
+flowers.pop() #popmethod
+print(flowers)
+
+flowers.pop(-1) #pop method with index
+print(flowers)
+
+print(flowers.count('rose')) # count method
+
+print(flowers.index('jasmine')) #index method
+
+flowers.reverse() #reverse method
+print(flowers)
+
+flowers.insert(2,'lavender') #insert method
+print(flowers)
+
+flowers.sort() #sort method
+print(flowers)
