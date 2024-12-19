27312fc240ee1839d2d6ef7def9143c6ea3ca9c2
AshaS591 <ashas17205@gmail.com>
2024-12-19 23:00:28 +0530

    list builtin methods

--- List/list.py
+++ List/list.py
@@ -1,4 +1,9 @@
 collection=[]
 for num in range(10):
-    collection.append(num)
+    collection.append(num) #append method
 print(collection)
+
+flowers=['jasmine','lily','sun flower','marrigold']
+new_flowers=flowers.copy() #copy method
+print(new_flowers)
+
