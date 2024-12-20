99c0664d96603afa2e5086c3ba83a2d9d9cee6d0
AshaS591 <ashas17205@gmail.com>
2024-12-20 21:15:44 +0530

    factorial program

--- functions/nested_functions.py
+++ functions/nested_functions.py
@@ -4,3 +4,15 @@ def outer():
     return inner
 var=outer()
 var()
+
+def number(num):
+    def factorial():
+        fact=1
+        for number in range(1,num+1):
+            fact*=number
+        print(f'Factorial of {num} is {fact}')
+    return factorial
+factorial_no=number(7)
+factorial_no()
+       
+   
