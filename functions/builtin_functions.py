6078302f202d28ab154d12d0c364924d3a5bd0a7
AshaS591 <ashas17205@gmail.com>
2024-12-20 21:05:57 +0530

    added few lines

--- functions/builtin_functions.py
+++ functions/builtin_functions.py
@@ -2,3 +2,13 @@ import math
 
 num=int(input('Enter a number :'))
 print(math.factorial(num))
+
+print(math.exp(4))
+
+print(math.gcd(4,9))
+
+print(math.cbrt(27))
+
+print(math.sqrt(100))
+
+print(math.fabs(-90))
