a3f0b09c06cf3f381562c36ac5715c8cb29d6015
AshaS591 <ashas17205@gmail.com>
2024-12-20 21:56:36 +0530

    ..

--- tuple/tuple.py
+++ tuple/tuple.py
@@ -2,8 +2,8 @@
 #we can check length and count of element
 
 lap=('dell','lenevo','hp','mac','asus')
-print(lap.count('dell'))
-print(lap.index('mac'))
+print(lap.count('dell'))  #count method
+print(lap.index('mac'))   #index method
 print(lap.index('mac',1,4))
 rings=('gold','silver','bronze','copper','platinum','diamond')
 print(len(rings))
