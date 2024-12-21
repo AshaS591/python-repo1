41041e81c7598b56987491adb41d81daec03782d
AshaS591 <ashas17205@gmail.com>
2024-12-21 21:42:10 +0530

    added few more programs

--- practice/Daily_practice.py
+++ practice/Daily_practice.py
@@ -565,3 +565,4 @@ print(common_elements)
 Get only the numbers in a sentence like ‘In 1984 there were 13 instances of a protest with over 1000 people attending’
 '''
 sentence='In 1984 there were 13 instances of a protest with over 1000 people attending'
+list
