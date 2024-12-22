19f77827914b141b864a500a3390b1d213e44f68
AshaS591 <ashas17205@gmail.com>
2024-12-22 20:02:26 +0530

    one ore program on abstraction

--- oops/abstraction.py
+++ oops/abstraction.py
@@ -20,3 +20,21 @@ class Lamorghini(Car):
 car=Lamorghini()
 car.accelerator()
 # car=Car() Can't instantiate abstract class Car without an implementation for abstract methods 'accelerator', 'brake'
+
+#############################################################################################
+
+class Python(ABC):
+    def welcome_msg(self):
+        print('Welcome to OOPS concepts....')
+        ...
+    @abstractmethod
+    def advance_topics(self):
+        ...
+class Advance(Python):
+    def welcome_msg(self):
+        print('welcome to advance concepts')
+    def advance_topics(self):
+       print('1.Exception Handling\n2.Comprehension\n3.Generators\n4.Decorators\n5.Regular Expressions')
+python=Advance()
+python.welcome_msg()
+python.advance_topics()
