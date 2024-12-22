4b25860379645b6188ec2af3b755059b8db7b673
AshaS591 <ashas17205@gmail.com>
2024-12-22 21:52:40 +0530

    few more examples

--- operators/assignment_operators.py
+++ operators/assignment_operators.py
@@ -11,8 +11,16 @@ print(num2) #12.7
 
 '''+= on strings'''
 name='Asha'
-name+=' S'
+name+=' S' #performs concatenation
 print(name) #=>Asha S
 
 '''+= on list'''
+names=['asha','shashi','abhi']
+names+=['arya'] #performs concatenation
+print(names) #['asha', 'shashi', 'abhi', 'arya'] 
+
+'''+= on tuple'''
+numbers=(10,20,90)
+numbers+=(30,70) #performs concatenation
+print(numbers) #(10, 20, 90, 30, 70)
 
