15480a3c671002ec9673a44b04a38f5deff77a6b
AshaS591 <ashas17205@gmail.com>
2024-12-22 09:27:25 +0530

    inheritance

--- oops/oops.py
+++ oops/oops.py
@@ -78,4 +78,39 @@ math.diff()
 math.div()
 math.product()
 math.mod()
+
+###############################################################################
+
+class Shapes:
+    def area(self):
+        print('This is Area method...')
+    def perimeter(self):
+        print('This is perimeter method...')
+class Circle(Shapes):
+    def area(self,radius):
+        self.area=3.14 * radius**2
+        print('Area of circle is :',self.area)
+    def perimeter(self,radius):
+       self.perimeter=2 * 3.14 *radius
+       print('Perimeter of circle is :',self.perimeter)
+class Square(Shapes):
+    def area(self,side):
+        self.area=side ** 2
+        print('Area of circle is :',self.area)
+    def perimeter(self,side):
+       self.perimeter= 4 *side
+       print('Perimeter of circle is :',self.perimeter)
+shape=Shapes()
+shape.area()
+shape.perimeter()
+circle1=Circle()
+circle1.perimeter(10)
+circle1.area(10)
+square=Square()
+square.area(10)
+square.perimeter(4)
+
+
+
         
+    
