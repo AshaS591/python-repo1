633936dfb51b3b8731e7ba64413f3690b58dee2b
AshaS591 <ashas17205@gmail.com>
2024-12-22 22:05:38 +0530

    += operator is done

--- operators/assignment_operators.py
+++ operators/assignment_operators.py
@@ -2,17 +2,26 @@
 """ += ,-= ,*= ,/= ,%= ,//= """
 
 '''+= on numbers'''
+
+'''int'''
 num=10
 num+=30 # =>num=num+10
 print(num) #40
 
+'''float'''
 num2=9.9
 num2+=2.8
 print(num2) #12.7
 
+'''complex'''
 complex1=2+3j
 complex1+=1+9j
 print(complex1) #(3+12j)
+
+'''bool'''
+truth=True #True =>1 and False =>0
+truth+=False  # 1+0 => 1
+print(truth) # 1
 
 '''+= on strings'''
 name='Asha'
@@ -29,6 +38,14 @@ numbers=(10,20,90)
 numbers+=(30,70) #performs concatenation
 print(numbers) #(10, 20, 90, 30, 70)
 
+'''+= on dictionary'''
 info={'name':'asha'}
 # info+={'age':21}
 # print(info) unsupported operand type(s) for +=: 'dict' and 'dict'
+
+'''+= on set'''
+set1={12,23}
+# set1+={4,5}
+# print(set1)  unsupported operand type(s) for +=: 'set' and 'set'
+
+##############################################################################################
