9f6a97618ef35c795334407675a8f21306b5abd2
AshaS591 <ashas17205@gmail.com>
2024-12-22 21:48:16 +0530

    assignment operators

---
+++ operators/assignment_operators.py
@@ -0,0 +1,17 @@
+''' Assignment Operators '''
+
+'''+= on numbers'''
+num=10
+num+=30 # =>num=num+10
+print(num) #40
+
+num2=9.9
+num2+=2.8
+print(num2) #12.7
+
+'''+= on strings'''
+name='Asha'
+name+=' S'
+print(name) #=>Asha S
+
+'''+= on '''
