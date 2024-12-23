d51aaec44336a22ab0373139952b8e28f0e8f0a9
AshaS591 <ashas17205@gmail.com>
2024-12-23 22:32:42 +0530

    ....

--- variable_scope/variable_scope.py
+++ variable_scope/variable_scope.py
@@ -1,64 +1,64 @@
-# num=20
-# def display(num):
-#     print(num)
-# display(num)
+num=20
+def display(num):
+    print(num)
+display(num)
 
-# num=10
-# def display():
-#     num=30
-#     print(num)
-#     def get_num():
-#         #print(num)#---->throws error i.e cannot access local variable 'num' where it is not associated with a value
-#         num=20
-#         num+=20
-#         print(num)
-#     print(num)
-#     get_num()
-# print(num)
-# display()
+num=10
+def display():
+    num=30
+    print(num)
+    def get_num():
+        #print(num)#---->throws error i.e cannot access local variable 'num' where it is not associated with a value
+        num=20
+        num+=20
+        print(num)
+    print(num)
+    get_num()
+print(num)
+display()
 
-# name='asha'
-# def display():
-#     # print(name)---->throws error i.e cannot access local variable 'num' where it is not associated with a value
-#     # name+=" s" ---->throws error i.e cannot access local variable 'num' where it is not associated with a value
-#     name='Asha'
-#     print(name)
-#     def display_name():
-#         global name
-#         name='Asha S'
-#         print(name)
-#     print(name)
-#     display_name()
-# display()
-# print(name)
+name='asha'
+def display():
+    # print(name)---->throws error i.e cannot access local variable 'num' where it is not associated with a value
+    # name+=" s" ---->throws error i.e cannot access local variable 'num' where it is not associated with a value
+    name='Asha'
+    print(name)
+    def display_name():
+        global name
+        name='Asha S'
+        print(name)
+    print(name)
+    display_name()
+display()
+print(name)
 
-# items=[10,20]
-# def display():
-#     global items
-#     items.append(40)
-#     print(items)
-#     def display_items():
-#         # print(items)
-#         items=[10,46,60]
-#         print(items)
-#     display_items()   
-# display()
-# print(items)
+items=[10,20]
+def display():
+    global items
+    items.append(40)
+    print(items)
+    def display_items():
+        # print(items)
+        items=[10,46,60]
+        print(items)
+    display_items()   
+display()
+print(items)
 
-# num=30
-# def display():
-#     print(num)
-# display()
+num=30
+def display():
+    print(num)
+display()
 
-# num1=20
-# num2=30
-# def outer():
-#     print(num1)
-#     num1=90
-#     def inner():
-#         print(num1)
-#     inner()
-# outer()
+num1=20
+num2=30
+def outer():
+    print(num1)
+    num1=90
+    def inner():
+        print(num1)
+    inner()
+outer()
 
 num=10
 def outer():
