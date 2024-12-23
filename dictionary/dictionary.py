da5bf433cbecee93e03d80f70d35bb27c74a1dcd
AshaS591 <ashas17205@gmail.com>
2024-12-23 22:45:30 +0530

    added few properties of dictionary

---
+++ dictionary/dictionary.py
@@ -0,0 +1,6 @@
+'''Ordered collection
+   1.data stored in the form of key-value pair
+   2.Each key,value are separated by :
+   3.key value pair separated by comma
+   4.does not store duplicate key
+   5. keys should of immutable datatype and values can be any datatype'''
