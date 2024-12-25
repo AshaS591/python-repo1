691661591c00e5a57b0a7871839df4d3fb00abba
AshaS591 <ashas17205@gmail.com>
2024-12-25 08:15:31 +0530

    ....

---
+++ Mini_Projects/data.py
@@ -0,0 +1,25 @@
+data=[
+    {
+        'name':'ISRO',
+        'followers':250,
+        'description':'space agency',
+        'country':'India'
+    },
+    {
+        'name':'Virat Kohli',
+        'followers':550,
+        'description':'Cricker',
+        'country':'India'
+    },
+    {'name':'ISRO',
+        'followers':250,
+        'description':'space agency',
+        'country':'India'
+    },
+    {
+        'name':'',
+        'followers':250,
+        'description':'space agency',
+        'country':'India'
+    }
+]
