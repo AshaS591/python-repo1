ae892c9867ec628e6c9a93bb54ff1d440679a585
AshaS591 <ashas17205@gmail.com>
2024-12-25 21:22:02 +0530

    dictionary

--- dictionary/dictionary.py
+++ dictionary/dictionary.py
@@ -1,6 +1,9 @@
-'''Ordered collection
+'''
+   Ordered collection
    1.data stored in the form of key-value pair
    2.Each key,value are separated by :
    3.key value pair separated by comma
    4.does not store duplicate key
-   5. keys should of immutable datatype and values can be any datatype'''
+   5. keys should of immutable datatype and values can be any datatype
+'''
+
