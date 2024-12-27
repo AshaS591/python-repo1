1a56e7dd092156a476f651da4f26eda7f9a3fc5b
AshaS591 <ashas17205@gmail.com>
2024-12-27 22:19:06 +0530

    one more program

--- Arguments/keyword_arguments.py
+++ Arguments/keyword_arguments.py
@@ -3,5 +3,9 @@ def sum(num1,num2):
 sum(num2=20,num1=25)
 
 def product(num1,num2):
-    print(f" Sum of {num1} and {num2} is {num1*num2}")
-product(num2=20,num1=25)
+    print(f" Product of {num1} and {num2} is {num1*num2}")
+product(num2=6,num1=9)
+
+def square(num1,num2):
+    print(f" Square of {num1} and {num2} is {num1**2},{num2**2}")
+square(num2=3,num1=12)
