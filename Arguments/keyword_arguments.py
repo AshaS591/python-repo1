3d4351e8ae98c82efa9cb67b1144e7707c768908
AshaS591 <ashas17205@gmail.com>
2024-12-27 22:16:48 +0530

    keyword arguments

--- Arguments/keyword_arguments.py
+++ Arguments/keyword_arguments.py
@@ -1,3 +1,7 @@
 def sum(num1,num2):
     print(f" Sum of {num1} and {num2} is {num1+num2}")
 sum(num2=20,num1=25)
+
+def product(num1,num2):
+    print(f" Sum of {num1} and {num2} is {num1*num2}")
+product(num2=20,num1=25)
