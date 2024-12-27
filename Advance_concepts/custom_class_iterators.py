238b3842c2a47700a57400392ae9aaf5a16d72b8
AshaS591 <ashas17205@gmail.com>
2024-12-27 22:14:36 +0530

    one more program

--- Advance_concepts/custom_class_iterators.py
+++ Advance_concepts/custom_class_iterators.py
@@ -50,3 +50,31 @@ print(next(obj))
 print(next(obj))
 print(next(obj))
 # print(next(obj)) StopIteration
+
+''' 3. '''
+class Nums:
+    def __init__(self,collection,num):
+        self.collection=collection
+        self.num=num
+        self.index=0
+    def __iter__(self):
+        return self
+    def __next__(self):
+
+        if self.index<len(self.collection):
+            value=self.collection[self.index]+self.index
+            self.index+=1
+            return value
+        
+        else:
+            raise StopIteration
+        
+items=[23,4,58,9,1]
+num=20
+obj=Add(items,num)
+print(next(obj))
+print(next(obj))
+print(next(obj))
+print(next(obj))
+print(next(obj))
+# print(next(obj)) StopIteration
