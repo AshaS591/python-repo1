48f3dc7d800a15db55430c4fa599140e088c4a4a
AshaS591 <ashas17205@gmail.com>
2024-12-27 22:05:58 +0530

    one more program

--- Advance_concepts/custom_class_iterators.py
+++ Advance_concepts/custom_class_iterators.py
@@ -1,3 +1,7 @@
+
+''' Custom Class Iterators'''
+
+''' 1 '''
 class Add:
     def __init__(self,collection,num):
         self.collection=collection
@@ -12,6 +16,31 @@ class Add:
             return value
         else:
             raise StopIteration
+items=[23,4,58,9,1]
+num=20
+obj=Add(items,num)
+print(next(obj))
+print(next(obj))
+print(next(obj))
+print(next(obj))
+print(next(obj))
+# print(next(obj)) StopIteration
+
+''' 2. '''
+class Multiply:
+    def __init__(self,collection,num):
+        self.collection=collection
+        self.num=num
+        self.index=0
+    def __iter__(self):
+        return self
+    def __next__(self):
+        if self.index<len(self.collection):
+            value=self.collection[self.index]*self.num
+            self.index+=1
+            return value
+        else:
+            raise f'No Number to multiply'
 items=[23,4,58,9,1]
 num=20
 obj=Add(items,num)
