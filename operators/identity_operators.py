c5e851ab2f14dbe27bad2dc5b176cb2e24b2a940
AshaS591 <ashas17205@gmail.com>
2024-12-28 21:49:58 +0530

    identity operators

---
+++ operators/identity_operators.py
@@ -0,0 +1,27 @@
+''' 
+Identity operators --->returns boolean values
+
+1.is
+2.is not
+
+ '''
+
+num1=10
+num2=10
+
+print(num1 is num2)
+
+print(num1 is not num2)
+
+num1=10
+num2=10.0
+print(num1 is not num2)
+
+num1=10.0
+num2=10.0
+print(num1 is not num2)
+
+num1=10.0
+num2=10.0
+print(num1 is num2)
+
