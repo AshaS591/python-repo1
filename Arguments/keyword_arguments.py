2d293f345ff33b278543f395d3f89a88ce01182e
AshaS591 <ashas17205@gmail.com>
2024-12-28 21:34:19 +0530

    ...

--- Arguments/keyword_arguments.py
+++ Arguments/keyword_arguments.py
@@ -1,3 +1,6 @@
+
+''' Parameter Names are used to pass the argument during the function call. '''
+
 def sum(num1,num2):
     print(f" Sum of {num1} and {num2} is {num1+num2}")
 sum(num2=20,num1=25)
