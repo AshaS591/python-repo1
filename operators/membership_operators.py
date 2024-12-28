9b08af361bbdcd637b94dc4b0134e6a3c0e25c5a
AshaS591 <ashas17205@gmail.com>
2024-12-28 21:45:51 +0530

    membership operators

---
+++ operators/membership_operators.py
@@ -0,0 +1,20 @@
+'''
+Membership operators ---> returns boolean values
+
+1.in
+2.not in
+
+'''
+
+collection=[10,0,45,'hello',90.9,45]
+
+print(10 in collection)
+
+print(90 in collection)
+
+print(90 not in collection)
+
+print('hello' in collection)
+
+print('hi' not in collection)
+
