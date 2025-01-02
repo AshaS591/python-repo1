afeabef279fd4020275c0e8b643ab6ad5222dd58
AshaS591 <ashas17205@gmail.com>
2025-01-02 21:39:11 +0530

    prime numbers using custom class iterator

--- Advance_concepts/custom_class_iterators.py
+++ Advance_concepts/custom_class_iterators.py
@@ -78,3 +78,30 @@ print(next(obj))
 print(next(obj))
 print(next(obj))
 # print(next(obj)) StopIteration
+
+
+class prime_inf:
+    def __init__(self):
+        self.num=2
+    def __iter__(self):
+        return self
+    def __next__(self):
+       
+        while True:
+            for num in range(2,int(self.num**0.5)+1):
+                if self.num%num==0:
+                    break
+            else:
+                
+                value=self.num
+                self.num+=1
+                return value
+            self.num+=1
+prime=prime_inf()
+print(next(prime))
+print(next(prime))
+print(next(prime))
+print(next(prime))
+print(next(prime))
+print(next(prime))
+
