2725d6107e55f7157cbd9b4142529e73696dfe66
AshaS591 <ashas17205@gmail.com>
2025-01-04 21:39:18 +0530

    2 more programs

--- Advance_concepts/map_and_filter.py
+++ Advance_concepts/map_and_filter.py
@@ -1,6 +1,19 @@
-''' Map function to print cube of each number of a collection'''
+''' Map function to find cube of each number of a collection'''
 def cube(num):
     return num**3
 nums=[10,20,21,90,15]
 res=map(cube,nums)
 print(next(res))
+
+''' map function to find square of each number of a coolection'''
+numbers=[2,3,4,5,9]
+res=map(lambda num:num**2,numbers)
+print(list(res))
+
+''' map function to find factorial of each number of a coolection'''
+
+numbers=[2,3,5,6]
+from math import factorial
+res=map(factorial,numbers)
+for num in res:
+    print(num)
