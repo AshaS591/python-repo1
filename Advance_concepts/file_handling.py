0173e6b9d5c769f3f08cce6874afe017d7a573a5
AshaS591 <ashas17205@gmail.com>
2025-01-05 20:47:15 +0530

    creating reading writing modifying the files

--- Advance_concepts/file_handling.py
+++ Advance_concepts/file_handling.py
@@ -1,7 +1,35 @@
 # file=open('movies.txt','x')
 # file.close()
 
-file=open('movies.txt','a')
-file.writelines(['3.lakki bhaskar\n','4.max\n'])
+# file=open('movies.txt','w')
+# file.writelines(['1.kantara\n','2.bahubali\n'])
 
-file.close()
+# file.close()
+
+# file=open('movies.txt','a')
+# file.writelines(['3.lakki bhaskar\n','4.max\n'])
+
+# file.close()
+
+# file=open('movies.txt','r')
+# print(file.read())
+
+# file.close()
+
+# file=open('movies.txt','r')
+# print(file.read(20))
+
+# file.close()
+
+# file=open('movies.txt','r')
+# print(file.readline())
+# print(file.readline())
+# print(file.readline())
+# print(file.readline())
+
+
+# file.close()
+
+# file=open('movies.txt','r')
+# print(file.readlines())
+# file.close()
