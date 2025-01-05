572251cb54d684972ad0f050701ace207d329fcf
AshaS591 <ashas17205@gmail.com>
2025-01-05 20:53:54 +0530

    steps

--- Advance_concepts/file_handling.py
+++ Advance_concepts/file_handling.py
@@ -1,16 +1,22 @@
+''' Creating text file'''
 # file=open('movies.txt','x')
 # file.close()
+
+''' writing into the text file'''
 
 # file=open('movies.txt','w')
 # file.writelines(['1.kantara\n','2.bahubali\n'])
 
 # file.close()
 
+''' appending new data into the text file at end'''
+
 # file=open('movies.txt','a')
 # file.writelines(['3.lakki bhaskar\n','4.max\n'])
 
 # file.close()
 
+''' reading from the file '''
 # file=open('movies.txt','r')
 # print(file.read())
 
